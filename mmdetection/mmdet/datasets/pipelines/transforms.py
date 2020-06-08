1825af4c6440ffa72c24fb2eac0f95a669fa3641
yuyijie1995 <13588280094@163.com>
2020-06-08 15:37:00 +0800

    add gridmask

--- mmdetection/mmdet/datasets/pipelines/transforms.py
+++ mmdetection/mmdet/datasets/pipelines/transforms.py
@@ -1,6 +1,7 @@
 import inspect
 
 import mmcv
+import torch
 import numpy as np
 from numpy import random
 from PIL import Image
@@ -1803,3 +1804,75 @@ class ResizeMasaic(object):
                                               self.ratio_range,
                                               self.keep_ratio)
         return repr_str
+@PIPELINES.register_module
+class GridMask(object):
+    def __init__(self, use_h=True, use_w=True, rotate=1, offset=False, ratio=0.5, mode=0, prob=0.7):
+        self.use_h = use_h
+        self.use_w = use_w
+        self.rotate = rotate
+        self.offset = offset
+        self.ratio = ratio
+        self.mode = mode
+        self.st_prob = prob
+        self.prob = prob
+
+
+    def set_prob(self, epoch, max_epoch):
+        self.prob = self.st_prob * epoch / max_epoch
+
+    def __call__(self, results):
+        if np.random.rand() > self.prob:
+            return results
+        img,boxes=[results[k] for k in ('img','gt_bboxes')]
+        h,w,_=img.shape
+        # h = img.size(1)
+        # w = img.size(2)
+        self.d1 = 2
+        self.d2 = min(h, w)
+        hh = int(1.5 * h)
+        ww = int(1.5 * w)
+        d = np.random.randint(self.d1, self.d2)
+        # d = self.d
+        #        self.l = int(d*self.ratio+0.5)
+        if self.ratio == 1:
+            self.l = np.random.randint(1, d)
+        else:
+            self.l = min(max(int(d * self.ratio + 0.5), 1), d - 1)
+        mask = np.ones((hh, ww), np.float32)
+        st_h = np.random.randint(d)
+        st_w = np.random.randint(d)
+        if self.use_h:
+            for i in range(hh // d):
+                s = d * i + st_h
+                t = min(s + self.l, hh)
+                mask[s:t, :] *= 0
+        if self.use_w:
+            for i in range(ww // d):
+                s = d * i + st_w
+                t = min(s + self.l, ww)
+                mask[:, s:t] *= 0
+
+        r = np.random.randint(self.rotate)
+        mask = Image.fromarray(np.uint8(mask))
+        mask = mask.rotate(r)
+        mask = np.asarray(mask)
+        #        mask = 1*(np.random.randint(0,3,[hh,ww])>0)
+        mask = mask[(hh - h) // 2:(hh - h) // 2 + h, (ww - w) // 2:(ww - w) // 2 + w]
+
+        # mask = torch.from_numpy(mask).float()
+        if self.mode == 1:
+            mask = 1 - mask
+
+        # mask = mask.expand_as(img)
+        mask=mask[:,:,np.newaxis]
+        mask=np.repeat(mask,3,axis=2)
+        # print('mask shape:', mask.shape)
+        # print('img shape:', img.shape)
+        if self.offset:
+            offset = torch.from_numpy(2 * (np.random.rand(h, w) - 0.5)).float()
+            offset = (1 - mask) * offset
+            img = img * mask + offset
+        else:
+            img = img * mask
+        results['img']=img
+        return results
